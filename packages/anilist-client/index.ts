b6fe1648a3097f1d94b8014fba82c2b927a70391
ahm4dd <ahm4dd@proton.me>
2026-03-31 16:06:52 +0300

    refactor(anilist-client): rename index.ts for client to client.ts

--- packages/anilist-client/index.ts
+++ packages/anilist-client/index.ts
@@ -1,14 +1,14 @@
-export { AnilistClient } from "./src/client/index.js";
-export { resolveAnilistClientConfig } from './src/client/bootstrap.js';
+export { AnilistClient } from "./src/client/client.js";
+export { resolveAnilistClientConfig } from "./src/client/bootstrap.js";
 export {
   HTTPClientAbortError,
   HTTPClientError,
   HTTPClientTimeoutError,
-} from './src/client/errors.js';
-export type { AnilistClientConfig } from './src/types/client.js';
-export type { AnilistClientLogger } from './src/types/logger.js';
+} from "./src/client/errors.js";
+export type { AnilistClientConfig } from "./src/types/client.js";
+export type { AnilistClientLogger } from "./src/types/logger.js";
 export type {
   GraphQLRequestOptions,
   GraphQLResponse,
   HTTPConfig,
-} from './src/types/httpclient.js';
+} from "./src/types/httpclient.js";
